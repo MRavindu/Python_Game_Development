800480a80c1b8bd321c2594e56ce5b4e4ebee51a
Ravindu <ravindumedagama@gmail.com>
2023-06-05 19:09:21 +0530

    Game Over Text added

--- main.py
+++ main.py
@@ -60,9 +60,19 @@ font = pygame.font.Font('freesansbold.ttf', 32)
 textX = 10
 textY = 10
 
+# Game over text
+over_font = pygame.font.Font('freesansbold.ttf', 64)
+
+
+def game_over_text():
+    over_text = over_font.render("GAME OVER!", True, (255, 255, 255))
+    screen.blit(over_text, (200, 250))
+
+
 def show_score(x, y):
     score = font.render("Score:" + str(score_value), True, (255, 255, 255))
     screen.blit(score, (x, y))
+
 
 def player(x, y):
     screen.blit(playerImg, (x, y))
@@ -130,10 +140,16 @@ while running:
     elif playerX >= 736:
         playerX = 736
 
-
-
     # Calling for the enemy function
     for i in range(num_of_enemies):
+
+        # Game Over
+        if enemyY[i] > 200:
+            for j in range(num_of_enemies):
+                enemyY[j] = 2000
+            game_over_text()
+            break
+
         enemyX[i] += enemyX_change[i]
         # setup some boundaries for the enemy ship
         if enemyX[i] <= 0:
