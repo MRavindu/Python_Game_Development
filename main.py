380d62e8633820012e94eee1cdd837011cbde9df
Ravindu <ravindumedagama@gmail.com>
2023-06-05 15:04:15 +0530

    Added the fevicon icon of the game window

--- main.py
+++ main.py
@@ -6,9 +6,18 @@ pygame.init()
 # Create the Screen
 screen = pygame.display.set_mode((800, 600))
 
+# Title and Icon
+pygame.display.set_caption("Star Defender")
+icon = pygame.image.load('./images/project.png')
+pygame.display.set_icon(icon)
+
 # Game loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    # Screen colors
+    screen.fill((128, 128, 128))
+    pygame.display.update()
